3a568a00a3c1da998e58df81de59c94424227d2b
jojo <love_faye@live.cn>
2015-10-29 00:55:54 +0800

    bugs fixed

--- test/test.py
+++ test/test.py
@@ -14,13 +14,15 @@ if __name__ == "__main__":
 
     img = pyflow.flow_to_color(flow)
     pylab.imshow(img)
+    pylab.show()
 
     pylab.imshow(pyflow.show_color_scheme())
+    pylab.show()
 
     prev_frame = cv2.imread("frame10.png")
     next_frame = cv2.imread("frame11.png")
     prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
     next_gray = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
-    fbflow = cv2.calcOpticalFlowFarneback(prev_gray, next_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    fbflow = cv2.calcOpticalFlowFarneback(prev_gray, next_gray, 0.5, 3, 15, 3, 5, 1.2, 0)
 
     print pyflow.calc_epe_stat(fbflow, flow)
